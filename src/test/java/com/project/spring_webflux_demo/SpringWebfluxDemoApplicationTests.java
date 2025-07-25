7abf8c513a650cdc7e19917ad47d1943e8ad68f8
DKichukov <dzhamal.kichukov@gmail.com>
2025-07-25 20:36:00 +0300

    initial commit

---
+++ src/test/java/com/project/spring_webflux_demo/SpringWebfluxDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.spring_webflux_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
